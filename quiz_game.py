61d57af3f5f8808901398efeeec9ffd1537f0287
minhtihd <minh4102004@gmail.com>
2023-05-10 16:24:52 +0700

    First commit, simple quiz game program

--- quiz_game.py
+++ quiz_game.py
@@ -1,11 +1,33 @@
-def load_questions():
-    pass
+import csv
+
+listOfQuestions = []
+
+def load_questions(questionSet):
+    listOfQuestions = []
+    with open(questionSet,newline='') as csvfile:
+        my_reader = csv.reader(csvfile)
+    for row in my_reader:
+        listOfQuestions.append(row)
+    return listOfQuestions
+
 
 def ask_question(question):
-    pass
+    score = 0
+    for oneQuestion in listOfQuestions:
+       answer = input(oneQuestion)
+       if answer == oneQuestion["3"]:
+           score += 1
+           print("Right bro.")
+       else:
+           print("Wrong bro.")
+
+    return score    
+
 
 def run_quiz():
-    pass
+    global newScore
+    newScore = [ask_question(load_questions("quiz_questions.csv")),input("What is your name")]
+    print("This is your score: {newScore[1]}")
 
 
 if __name__ == "__main__":
